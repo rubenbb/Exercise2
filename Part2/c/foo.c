d8e2d1078c573476af7d326fd0c1997cc77cd453
rubenbb <36450070+rubenbb@users.noreply.github.com>
2019-01-23 10:32:01 +0100

    Update foo.c

--- Part2/c/foo.c
+++ Part2/c/foo.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t mutex;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
 	// TODO: sync access to i
+	pthread_mutex_lock(&mutex);
 	i++;
+	pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
@@ -15,7 +18,9 @@ void* incrementingThreadFunction(){
 void* decrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
 	// TODO: sync access to i
+	pthread_mutex_lock(&mutex);
 	i--;
+	pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
